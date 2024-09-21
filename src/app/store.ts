c32e330d670b20628ea8336765e62897ca3d3f1b
taserbeat <taserbeat@gmail.com>
2024-09-21 11:35:31 +0900

    feat: 現在のモードを管理するReducerを作成

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
 import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit";
 
 import counterReducer from "../features/counter/counterSlice";
+import modeReducer from "../features/mode/modeSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    mode: modeReducer,
   },
 });
 
---
+++ src/features/mode/modeSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+
+type ModeName = "Setting" | "Practice" | "Result";
+
+export interface ModeState {
+  /** モード名 */
+  name: ModeName;
+}
+
+/** 初期値 */
+const initialState: ModeState = {
+  name: "Setting",
+};
+
+/** スライス */
+export const modeSlice = createSlice({
+  name: "mode",
+  initialState: initialState,
+  reducers: {
+    /** 初期状態にする */
+    initialize: (state) => {
+      return initialState;
+    },
+    /** 指定したモードをセットする */
+    setMode: (state, action: PayloadAction<ModeName>) => {
+      state.name = action.payload;
+    },
+  },
+});
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// Actions
+
+export const { initialize, setMode } = modeSlice.actions;
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// Selector
+export const selectCurrentMode = (state: RootState) => state.mode.name;
+
+export default modeSlice.reducer;
